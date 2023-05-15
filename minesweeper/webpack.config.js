1756d00c34c1dab6cb39258ab7330525686ae108
eugenebalts <norcobeatz@gmail.com>
2023-05-15 15:49:26 +0200

    feat: install webpack and set config for it

---
+++ minesweeper/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+module.exports = {
+    // mode: 'development',
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            title: 'minesweeper',
+            template: './src/index.html'
+        }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            }
+        ]
+    }
+}
