006c7cfa18d72ba47fc450783ab2f4ec1f1ca967
eugenebalts <norcobeatz@gmail.com>
2023-05-20 18:59:23 +0200

    fix: fix bags in functions and replace it

--- minesweeper/src/index.js
+++ minesweeper/src/index.js
@@ -11,6 +11,18 @@ async function minesweeper(COLS, ROWS, BOMBS) {
     let field;
 
     const bombsArray = [];
+    const matrix = [];
+
+    function createMatrix(array) {
+        for(let i = 0; i < ROWS; i++){
+            matrix[i] = [];
+            for(let j = 0; j < COLS; j++){
+                matrix[i][j] = ((i * ROWS) + j);
+            }
+        }
+    }
+    createMatrix(matrix)
+    console.log(matrix)
 
     async function variables() {
         cells = document.querySelectorAll('.field__cell');
@@ -29,21 +41,23 @@ async function minesweeper(COLS, ROWS, BOMBS) {
             cell.classList.add('field__cell');
             cell.style.height = cellSize;
             cell.style.width = cellSize;
+            cell.style.fontWeight = '600'
             cell.setAttribute('number', i)
+            cell.setAttribute('opened', false)
     
             field.append(cell)
         }
         document.querySelector('body').append(field);
-    } 
+    }
 
-    async function fillBombs (bombs) {    
+    async function fillBombs (bombs) {   
         let isClicked = false;
-        let firstClick = 0;
+        let index = 0;
     
         async function randomize () {
             let randomNum = Math.floor(Math.random() * cells.length);
             for (let i =  bombsArray.length; i < BOMBS; i++) {
-                if (randomNum * 1 ===  firstClick * 1) {
+                if (randomNum * 1 ===  index * 1) {
                     randomize();
                 } else if (!bombsArray.includes(randomNum)) {  
                     bombsArray.push(randomNum);
@@ -52,31 +66,30 @@ async function minesweeper(COLS, ROWS, BOMBS) {
                 }
             }
             isClicked = true;
-            field.removeEventListener('click', listener);
         }
-    
+        let clicks = 0;
         async function listener (event) {
             if (event.target.classList.contains('field__cell')) {
-                firstClick = event.target.getAttribute('number');
-                event.target.style.backgroundColor = 'rgba(127, 255, 0, 0.5)'
-                await randomize();
-                handler();
+                index = parseInt(event.target.getAttribute('number'));
+                event.target.setAttribute('opened', true)
+                await randomize()
+                
+                clicks++
+                if (clicks == 1) await fillCells()
+                openCells(index, event)
+                }
             }
-        }
         field.addEventListener('click', listener);
     }
 
-    async function handler() {
-        console.log(bombsArray)
-        let counter = 0;
         let leftSide = [];
         let rightSide = [];
         let topSide = [];
         let downSide = [];
         let diagonals = [];
 
+    function fillCells() {
         for (let i = 0; i < bombsArray.length; i++) {
-
             let bombsLineLeft = (bombsArray[i] + 1) % (COLS) !== 0;
             let bombsLineRight = bombsArray[i] !== 0 && bombsArray[i] % COLS !== 0;
             let bombsLineTop = bombsArray[i] <= (COLS * ROWS) - COLS;
@@ -93,7 +106,6 @@ async function minesweeper(COLS, ROWS, BOMBS) {
             if (bombsLineDown) topSide.push(bombsArray[i] - COLS);
 
             if (bombsLineLeft || bombsLineRight || bombsLineTop || bombsLineDown) {
-
                 // DIAGONALS - bombsInCenter 
                 if (bombsLineLeft && bombsLineRight && bombsLineTop && bombsLineDown) {
                     diagonals.push(diagTopLeft, diagTopRight, diagDownLeft, diagDownRight)
@@ -124,57 +136,128 @@ async function minesweeper(COLS, ROWS, BOMBS) {
                 }
             }
         }
-
-        cells.forEach((item, index) => {
-            item.addEventListener('click', (event) => {
-                counter++;  
-                event.target.innerHTML = 0 * 1;
-                if (leftSide.includes(index) || rightSide.includes(index) || downSide.includes(index) || topSide.includes(index) || diagonals.includes(index) || bombsArray.includes(index)) {
-
-                    if(diagonals.includes(index)) {
-                        const diagCounts = diagonals.filter(item => item == index).length;
-                        event.target.innerHTML = parseInt(event.target.innerHTML) + diagCounts;
-                        item.style.backgroundColor = 'rgba(127, 255, 0, 0.5)'
-                    }
-
-                    if(topSide.includes(index)) {
-                        event.target.innerHTML = parseInt(event.target.innerHTML) + 1;
-                        item.style.backgroundColor = 'rgba(127, 255, 0, 0.5)'
-                    }
-
-                    if(downSide.includes(index)) {
-                        event.target.innerHTML = parseInt(event.target.innerHTML) + 1;
-                        item.style.backgroundColor = 'rgba(127, 255, 0, 0.5)'
-                    }
-
-                    if (leftSide.includes(index)) {
-                        event.target.innerHTML = parseInt(event.target.innerHTML) + 1;
-                        item.style.backgroundColor = 'rgba(127, 255, 0, 0.5)'
-                    }
-                    
-                    if(rightSide.includes(index)) {
-                        event.target.innerHTML = parseInt(event.target.innerHTML) + 1;
-                        item.style.backgroundColor = 'rgba(127, 255, 0, 0.5)'
-                    }
-
-                    if (bombsArray.includes(index)) {
-                        event.target.innerHTML = `<img src="${bombIcon}" alt="Bomb">`
-                        item.style.backgroundColor = 'rgba(255, 0, 0, 0.5)'
-                    }
-                    
-                } else {
-                    event.target.innerHTML = ' ';
-                    item.style.backgroundColor = 'rgba(127, 255, 0, 0.5)'
-                }
-            })
-        })
     }
 
+    function openCells(index, event) {
+        cells[index].innerHTML = 0;
+        
+
+        function stylizeButton(index) {
+            cells[index].innerHTML = parseInt(cells[index].innerHTML) + 1;
+            cells[index].style.backgroundColor = 'rgba(100, 100, 100, 0.3)'
+            cells[index].setAttribute('opened', true);
+        }
+
+        function diagonalButtons(index) {
+            const diagCounts = diagonals.filter(item => item == index).length;
+            console.log(diagCounts)
+            cells[index].innerHTML = parseInt(cells[index].innerHTML) + diagCounts;
+            cells[index].style.backgroundColor = 'rgba(100, 100, 100, 0.3)'
+            checkInner(index)
+        }
+
+        function setMarker() {
+            cells[index].setAttribute('hint', true);
+        }
+
+        async function emptyCell(index) {
+            cells[index].setAttribute('empty', true);
+            cells[index].innerHTML = ' ';
+            cells[index].style.backgroundColor = 'rgba(100, 100, 100, 0.3)';
+            // for (let i = 0; i < matrix.length; i++) {
+            //     for (let j = 0; j < matrix[i].length; j++) {
+            //         if (index == matrix[i][j]) console.log('index is', matrix[i][j], 'next is ' + matrix[i][j + 1]);
+            //         if (matrix[i][j + 1]) 
+            //     }
+            // }
+            // cells.forEach((item, i) => {
+            //     if (!(leftSide.includes(i) || rightSide.includes(i) || downSide.includes(i) || topSide.includes(i) || diagonals.includes(i) || bombsArray.includes(i))) {
+            //         item.style.backgroundColor = 'rgba(100, 100, 100, 0.3)';
+            //     }
+            // })
+
+            // (async function() {
+            //     for (let i = 0; i < matrix.length; i++) {
+            //         for (let j = 0; j < matrix[i].length; j++) {
+            //             if (matrix[i][j] == index) {
+            //                 if ((j < matrix[i].length - 1)) {
+            //                     if (!(leftSide.includes(index + 1) || rightSide.includes(index + 1) || downSide.includes(index + 1) || topSide.includes(index + 1) || diagonals.includes(index + 1) || bombsArray.includes(index + 1))) { 
+            //                         await emptyCell(matrix[i][j + 1])
+            //                     } else {
+            //                         cells[index + 1].innerHTML = 0;
+            //                         stylizeButton(matrix[i][j + 1]);
+            //                         if (diagonals.includes(index + 1)) diagonalButtons(index + 1)
+            //                     }
+            //                 }
+            //             }
+            //         }
+            //     }
+            // })()
+
+            // if (leftSide.includes(index + 1) || rightSide.includes(index + 1) || downSide.includes(index + 1) || topSide.includes(index + 1) || diagonals.includes(index + 1) || bombsArray.includes(index + 1)) {
+            //     cells[index + 1].innerHTML = 0;
+            //     stylizeButton(index + 1)
+            // } else await emptyCell(index + 1)
+
+            // if (leftSide.includes(index - 1) || rightSide.includes(index - 1) || downSide.includes(index - 1) || topSide.includes(index - 1) || diagonals.includes(index - 1) || bombsArray.includes(index - 1)) {
+            //     cells[index - 1].innerHTML = 0;
+            //     stylizeButton(index - 1)
+            // } else emptyCell(index - 1)
+            
+        }
+
+
+
+        if (leftSide.includes(index) || rightSide.includes(index) || downSide.includes(index) || topSide.includes(index) || diagonals.includes(index) || bombsArray.includes(index) ) {
+            if(diagonals.includes(index)) {
+                diagonalButtons(index)
+            }
+
+            if(topSide.includes(index)) {
+                stylizeButton(index)
+                setMarker(index)
+            }
+
+            if(downSide.includes(index)) {
+                stylizeButton(index)
+                setMarker(index)
+            }
+
+            if (leftSide.includes(index)) {
+                stylizeButton(index)
+                setMarker(index)
+            }
+            
+            if(rightSide.includes(index)) {
+                stylizeButton(index)
+                setMarker(index)
+            }
+
+            if (bombsArray.includes(index)) {
+                cells[index].innerHTML = `<img src="${bombIcon}" alt="Bomb">`
+                cells[index].style.backgroundColor = 'rgba(255, 0, 0, 0.5)'
+            }
+        } else {
+            emptyCell(index)
+            console.log('empty')
+        }
+        checkInner(index)
+    }
+
+    function checkInner(index) {
+        if (cells[index].innerHTML == 1) cells[index].style.color = 'blue';
+        if (cells[index].innerHTML == 2) cells[index].style.color = 'green';
+        if (cells[index].innerHTML == 3) cells[index].style.color = 'purple';
+        if (cells[index].innerHTML == 4) cells[index].style.color = 'Crimson';
+        if (cells[index].innerHTML == 5) cells[index].style.color = 'Red';
+        if (cells[index].innerHTML == 6) cells[index].style.color = 'FireBrick';
+        if (cells[index].innerHTML == 7) cells[index].style.color = 'DarkRed';
+        if (cells[index].innerHTML == 8) cells[index].style.color = 'brown';
+    }
     await createField(COLS, ROWS);
     await variables();
     await fillBombs(BOMBS);
     
-
 }
 
 minesweeper(5, 5, 5);
